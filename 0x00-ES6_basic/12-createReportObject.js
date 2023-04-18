9027bb1a02dc6f2e32423fa8f737c2b331a61efb
Geoffy1 <geojogu@gmail.com>
2023-04-18 20:04:18 +0000

    object literals  method

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,18 +1,8 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const department in employeesList) {
-    if (object.hasOwnProperty.call(employeesList, departmentName)) {
-	    allEmployees[department] = employeesList[department];
-    }
-  }
-
-  const resObject = {
+  return {
     allEmployees,
     getNumberOfDepartments() {
       return Object.keys(employeesList).length;
     },
   };
-
-  return resObject;
 }
