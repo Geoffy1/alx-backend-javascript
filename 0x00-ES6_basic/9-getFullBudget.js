99e20b4e5446ae48525955045198886e85c6d9bd
Geoffy1 <geojogu@gmail.com>
2023-04-18 19:54:32 +0000

    task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -2,7 +2,7 @@ import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  const myBudget = const fullBudget = {
+  const fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
       return `$${income}`;
@@ -12,5 +12,5 @@ export default function getFullBudgetObject(income, gdp, capita) {
     },
   };
 
-  return myBudget;
+  return fullBudget;
 }
