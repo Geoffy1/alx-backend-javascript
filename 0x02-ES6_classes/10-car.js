6fb7fbaf05a1c3762ac47842acb5c0ebf51acf0e
Geoffy1 <geojogu@gmail.com>
2023-04-20 23:15:19 +0000

    my task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,8 @@
-const cloneCarSymbol = Symbol('cloneCar');
-
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-    this[cloneCarSymbol] = this.cloneCar.bind(this);
   }
 
   get brand() {
