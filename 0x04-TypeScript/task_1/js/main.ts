0f3b4366edd4c194314f1ee55e3bf783ce8533a2
Geoffy1 <geojogu@gmail.com>
2023-04-28 10:36:55 +0000

    my typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,50 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 20,
+  location: "New York"
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 21,
+  location: "Los Angeles"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+
+studentsList.forEach((student) => {
+  const row = table.insertRow();
+  const firstNameCell = row.insertCell();
+  const locationCell = row.insertCell();
+  
+  firstNameCell.innerHTML = student.firstName;
+  locationCell.innerHTML = student.location;
+});
+
+document.body.appendChild(table);
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
