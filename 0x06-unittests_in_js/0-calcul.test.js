3dab793cacd065ae8da264329950858992f10a65
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-06-15 13:59:10 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,20 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', () => {
-  it('should return the sum of rounded numbers', () => {
+  it('floating point whole numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it("rounding down b's floating point fractional number", () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it("rounding down a and b's floating point fractional number", () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it("rounding down a's floating point fractional number", () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
