ffddaf6636e67a57e38fec0748868b6399e501e9
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-06-15 18:41:12 +0300

    Create 3-payment.test.js

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with the correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+});
