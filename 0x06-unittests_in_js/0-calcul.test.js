686d2f93cf6d45970bbd2fa927e28c09813d9f0a
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-06-16 12:03:45 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,5 @@
 const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
-describe('calculateNumber', () => {
-    it('rounding of a', () => {
-        assert.equal(calculateNumber(1, 3), 4);
-        assert.equal(calculateNumber(1, 3.7), 5);
-        assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
-
-    it('rounding of b', () => {
-        assert.equal(calculateNumber(3, 1), 4);
-        assert.equal(calculateNumber(3.7, 1), 5);
-        assert.equal(calculateNumber(3.7, 1.5), 6);
-    });
-
-    it('suming of a and b', () => {
-        assert.equal(calculateNumber(1, 3.5), 5);
-        assert.equal(calculateNumber(1, 3.7), 7;
-        assert.equal(calculateNumber(1.5, 3.7), 7);
-    });
-})
+assert.strictEqual(roundNumber(a));
+assert.strictEqual(roundNumber(b));
